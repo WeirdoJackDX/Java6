540f6e42e3dfc0dfa1fe548b47fdc79a0fc12feb
WeirdoJackDX <lhtho1803@gmail.com>
2023-08-07 15:45:21 +0700

    8/7/2023/ 15:45

--- Java6_Nhom2/src/main/java/com/poly/asm_nhom_6/controller/PaymentController.java
+++ Java6_Nhom2/src/main/java/com/poly/asm_nhom_6/controller/PaymentController.java
@@ -11,12 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
+import com.poly.asm_nhom_6.DAO.NguoiDungDAO;
 import com.poly.asm_nhom_6.config.PaypalPaymentIntent;
 import com.poly.asm_nhom_6.config.PaypalPaymentMethod;
+import com.poly.asm_nhom_6.model.NguoiDung;
 import com.poly.asm_nhom_6.service.PaypalService;
 import com.poly.asm_nhom_6.utils.Utils;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PaymentController {
@@ -28,6 +31,12 @@ public class PaymentController {
 	@Autowired
 	private PaypalService paypalService;
 
+	@Autowired
+	HttpSession session;
+
+	@Autowired
+	NguoiDungDAO nguoiDungDAO;
+
 	@GetMapping("/")
 	public String index() {
 		return "index";
@@ -37,21 +46,26 @@ public class PaymentController {
 	public String pay(HttpServletRequest request, @RequestParam("price") double price) {
 		String cancelUrl = Utils.getBaseURL(request) + "/" + URL_PAYPAL_CANCEL;
 		String successUrl = Utils.getBaseURL(request) + "/" + URL_PAYPAL_SUCCESS;
-		try {
-			Payment payment = paypalService.createPayment(price, "USD", PaypalPaymentMethod.paypal,
-					PaypalPaymentIntent.sale, "payment description", cancelUrl, successUrl);
-			for (Links links : payment.getLinks())
-				if (links.getRel().equals("approval_url"))
-					return "redirect:" + links.getHref();
-		} catch (PayPalRESTException e) {
-			log.error(e.getMessage());
+		NguoiDung user = (NguoiDung) session.getAttribute("nguoiDung");
+		user = nguoiDungDAO.findById(user.getMaND()).get();
+		if (user.getGioHangChiTiets().size() > 0) {
+			try {
+				price = price / 24000;
+				Payment payment = paypalService.createPayment(price, "USD", PaypalPaymentMethod.paypal,
+						PaypalPaymentIntent.sale, "payment description", cancelUrl, successUrl);
+				for (Links links : payment.getLinks())
+					if (links.getRel().equals("approval_url"))
+						return "redirect:" + links.getHref();
+			} catch (PayPalRESTException e) {
+				log.error(e.getMessage());
+			}
 		}
-		return "redirect:/";
+		return "redirect:/user/cart/index";
 	}
 
 	@GetMapping(URL_PAYPAL_CANCEL)
 	public String cancelPay() {
-		return "cancel";
+		return "redirect:/user/cart/index";
 	}
 
 	@GetMapping(URL_PAYPAL_SUCCESS)
@@ -59,10 +73,10 @@ public class PaymentController {
 		try {
 			Payment payment = paypalService.executePayment(paymentId, payerId);
 			if (payment.getState().equals("approved"))
-				return "success";
+				return "redirect:/user/cart/purchase";
 		} catch (PayPalRESTException e) {
 			log.error(e.getMessage());
 		}
-		return "redirect:/";
+		return "redirect:/user/cart/index";
 	}
 }
